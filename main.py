e805b13c46018cc00bd9ca6a1c755f7ff3d88d71
iq2v <142683864+iq2v@users.noreply.github.com>
2023-09-12 20:22:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-"person" = {1}
-"name": jana 
-"age": 16 
- "hobbies": list of strings 
-print("my name is jana") 
-print("my age is 16")
+person = {
+    "name":"jana",
+    "age":16,
+    "habbies":["Study"]
+}
+print(person["name"])
+print(person["age"])
 
+#الجزء الثاني
+person["age"] = 17
+person["newproperty"] = "America"
+print("person dictionary:",person)
+print("person dictionary:", len(person))
+
+#الجزء الثالث
+person["habbies"].append("Read")
+print("person dictionary", person)
+
+def check_hobbies(person):
+    if "hobbies" in person:
+        hobbies = person["hobbies"]
+        if len(hobbies) > 3:
+            person("wow you are amazing")
+        return hobbies
+    else:
+        return []
+
+person = {
+    "name": "jana",
+    "age" : 16,
+    "hobbies":["study,read"]
+}
+
+hobbies = check_hobbies(person)
+print("hobbies")
 
 
